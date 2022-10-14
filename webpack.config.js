e7e0c377fbaef942860383383daefd4567ae004f
harendra2691 <harendrasingh2691@gmail.com>
2022-10-14 18:38:44 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "production",
+  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       inject: "body",
